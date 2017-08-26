68cd46bb6b033ae6903328225cec0b0b8ef00c23
David Todd <delicasso@netscape.net>
2017-08-26 14:54:24 -0700

    hello world is good to go

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,1 +1,23 @@
+// Hello World for Windows.
 
+#include "stdafx.h" // comment out if not running on the Windows OS.
+#include <iostream>
+#include <string>
+
+void pause_function();
+
+int main(int argc, char** argv) {
+
+	std::string hw_ = "Hello World~!";
+
+	std::cout << hw_ << std::endl;
+
+	pause_function();
+	return 0;
+}
+
+void pause_function() {
+	char q_in;
+	std::cout << std::endl << "press 'q' and then Enter to continue: ";
+	std::cin >> q_in;
+}
