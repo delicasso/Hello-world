107fea336f7742644ddedf792ec0195257aa75c5
David Todd <delicasso@netscape.net>
2017-08-26 15:36:30 -0700

    Updated code and changed message to include 'big' and 'beautiful'

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,1 +1,23 @@
+// Hello World program for Windows.
 
+#include "stdafx.h" // Comment out if not using Windows OS
+#include <iostream>
+#include <string>
+
+void pause_function();
+
+int main(int argc, char** argv) {
+
+	std::string hw_ = "Hello Big Beautiful World~!";
+
+	std::cout << hw_ << std::endl;
+
+	pause_function();
+	return 0;
+}
+
+void pause_function() {
+	char q_in;
+	std::cout << std::endl << "Press 'q' and then Enter to continue: ";
+	std::cin >> q_in;
+}
